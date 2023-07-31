a83705465d8683f838ac20bd614244cabb5dadaa
leticia.chijo <leticia.chijo@gmail.com>
2023-07-31 19:25:17 -0300

    Requisitos: GET e POST da rota /games

--- src/schemas/games.schemas.js
+++ src/schemas/games.schemas.js
@@ -3,6 +3,6 @@ import joi from "joi"
 export const gameSchema = joi.object({
     name: joi.string().trim().required(),
     image: joi.string().uri().trim().required(),
-    stockTotal: joi.number().integer().min(1).required(),
-    pricePerDay: joi.number().integer().min(1).required()
+    stockTotal: joi.number().integer().min(1).required().strict(),
+    pricePerDay: joi.number().integer().min(1).required().strict()
 })
